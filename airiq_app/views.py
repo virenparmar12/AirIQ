056f792141717203142e5f2aaaa1d375c0b9ff53
virenparmar12 <virenparmar1912@gmail.com>
2021-05-24 16:35:16 +0530

    Homedata updated

--- airiq_app/views.py
+++ airiq_app/views.py
@@ -9,6 +9,7 @@ from pymodbus.constants import Endian
 from airiq_app.models import *
 from django.forms.models import model_to_dict
 import json
+from django.core import serializers
 # Create your views here.
 
 
@@ -16,8 +17,22 @@ def index(request):
     return render(request, 'index.html')
 
 def getData(request):
-    Home_data = HomeData.objects.all()[:20]
-    return JsonResponse({"Home_data":list(Home_data.values())[-1]})
+    latest_data_id = HomeData.objects.latest('id')
+    Home_data = HomeData.objects.get(id=latest_data_id.id)
+    response = {}
+    response["cr1"] = Home_data.cr1
+    response["cr2"] = Home_data.cr2
+    response["metal_loss1"] = Home_data.metal_loss1
+    response["metal_loss2"] = Home_data.metal_loss2
+    response["temprature"] = Home_data.temprature
+    response["differential_pressure"] = Home_data.differential_pressure
+    response["relative_humidity"] = Home_data.relative_humidity
+
+    return JsonResponse({"Home_data": response})
+
+
+def every_second(request):
+    pass
 
 def air_settings(request):
     return render(request, 'settings.html')
--- database_script.py
+++ database_script.py
@@ -1,5 +1,6 @@
 import os
 import django
+from time import sleep
 from pymodbus.client.sync import ModbusTcpClient
 from pymodbus.payload import BinaryPayloadDecoder, BinaryPayloadBuilder
 from pymodbus.constants import Endian
@@ -16,18 +17,22 @@ ip_address = "127.0.0.1"
 client = ModbusTcpClient(ip_address)
 
 client.connect()
-
-register_value_list = []
-x = 40021
-for i in range(7):
-    read_value = client.read_holding_registers(x, 2)
-    real_decoder = BinaryPayloadDecoder.fromRegisters(
-        read_value.registers, byteorder=Endian.Big, wordorder=Endian.Little)
-    value = real_decoder.decode_32bit_float()
-    register_value_list.append(value)
-    x += 2
-    # print(value)
-# print('%.2f' % register_value_list[4])
-homedata = HomeData(
-    differential_pressure='%.2f' % register_value_list[4], temprature=register_value_list[2], relative_humidity=register_value_list[3],metal_loss1=register_value_list[0],metal_loss2=register_value_list[1],cr1=register_value_list[5],cr2=register_value_list[6])
-homedata.save()
+try:
+    while True:
+        register_value_list = []
+        x = 40021
+        for i in range(7):
+            read_value = client.read_holding_registers(x, 2)
+            real_decoder = BinaryPayloadDecoder.fromRegisters(
+                read_value.registers, byteorder=Endian.Big, wordorder=Endian.Little)
+            value = real_decoder.decode_32bit_float()
+            register_value_list.append(value)
+            x += 2
+            # print(value)
+        # print('%.2f' % register_value_list[4])
+        homedata = HomeData(
+            differential_pressure='%.2f' % register_value_list[4], temprature=register_value_list[2], relative_humidity=register_value_list[3],metal_loss1=register_value_list[0],metal_loss2=register_value_list[1],cr1=register_value_list[5],cr2=register_value_list[6])
+        homedata.save()
+        sleep(0.5)
+finally:
+    client.close()
